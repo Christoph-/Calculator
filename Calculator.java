875ad7510e92b35da592479003e0afab242e201f
Christoph- <christophmennerich@yahoo.de>
2015-04-15 20:35:14 +0200

    adding two numbers

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,16 @@ public class Calculator{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Gib eine Rechnung der Form 'zahl1 +,-,*,/ zahl2' ein:");
-		
+
 		int number1 = scanner.nextInt();
 		String operator = scanner.next();
 		int number2 = scanner.nextInt();
+		int result = 0;
+
+		if(operator.equals("+")){
+			result = number1 + number2;
+			System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+		}
+
 	}
 }
