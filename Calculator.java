0fa198e1ec1c53ad5cc2138cc20c203dce0c9fdf
Christoph- <christophmennerich@yahoo.de>
2015-04-15 20:23:24 +0200

    added user input

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,10 @@ import java.util.*;
 public class Calculator{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Gib eine Rechnung der Form 'zahl1 +,-,*,/ zahl2' ein:");
+		
+		int number1 = scanner.nextInt();
+		String operator = scanner.next();
+		int number2 = scanner.nextInt();
 	}
 }
